57afef319a488c765e2a8b3771049e65917eed5e
Adiwijaya76 <insaya55@gmail.com>
2023-01-10 13:59:47 +0700

    first commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
+$$route['default_controller'] = 'Login';
+$route['404_override'] = 'Pagesystem';
 $route['translate_uri_dashes'] = FALSE;
